735644fed3b80bac91cddd80de281307f518de0c
contaxtdave <david.ross@contaxtsolutions.com>
2025-11-06 11:36:50 -0500

    moved data files to data folder

--- chapter_4/extraction/extraction_functional_enhanced.py
+++ chapter_4/extraction/extraction_functional_enhanced.py
@@ -74,7 +74,7 @@ def source_data_from_webpage(web_page_url, matching_keyword):
 def extracted_data():
     parquet_file_name = "chapter_4/data/yellow_tripdata_2025-01.parquet"
     csv_file_name = "chapter_4/data/yellow_tripdata_sample.csv"
-    api_endpoint = "http://data.cityofnewyork.us/resource/hgi-nx95.json?$limit=500"
+    api_endpoint = "https://data.cityofnewyork.us/resource/hgi-nx95.json?$"limit"
     db_name = "chapter_4/data/movies.sqlite"
     table_name = "movies"
     web_page_url = "https://en.wikipedia.org/wiki/List_of_countries_by_GOP_(nominal)"
