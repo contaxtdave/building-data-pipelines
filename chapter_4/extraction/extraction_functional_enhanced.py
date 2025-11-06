2d8deff832b6f14f83fcfb907303b7e29d3b98bf
contaxtdave <david.ross@contaxtsolutions.com>
2025-11-06 11:29:27 -0500

    more fixes

--- chapter_4/extraction/extraction_functional_enhanced.py
+++ chapter_4/extraction/extraction_functional_enhanced.py
@@ -72,10 +72,10 @@ def source_data_from_webpage(web_page_url, matching_keyword):
     return df_html
 
 def extracted_data():
-    parquet_file_name = "yellow_tripdata_2025-01.parquet"
-    csv_file_name = "yellow_tripdata_sample.csv"
-    api_endpoint = "https://data.cityofnewyork.us/resource/gkne-dk5s.json?$limit=500"
-    db_name = "movies.sqlite"
+    parquet_file_name = "chapter_4/data/yellow_tripdata_2025-01.parquet"
+    csv_file_name = "chapter_4/data/yellow_tripdata_sample.csv"
+    api_endpoint = "http://data.cityofnewyork.us/resource/hgi-nx95.json?$limit=500"
+    db_name = "chapter_4/data/movies.sqlite"
     table_name = "movies"
     web_page_url = "https://en.wikipedia.org/wiki/List_of_countries_by_GOP_(nominal)"
     matching_keyword = "by country"
